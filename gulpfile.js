7f31442768a120f30c85f9ccfe2537c171de58f5
Ruslan Yuldashev <monitoringe@gmail.com>
2015-09-22 21:25:16 +0300

    Add SCSS Support

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('sass', function () {
+  gulp.src('./src/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/*.scss', ['sass']);
 });
