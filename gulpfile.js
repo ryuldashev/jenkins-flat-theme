af7ead45ec7fc970e1c108b7639338b8d58e505c
Ruslan Yuldashev <monitoringe@gmail.com>
2015-09-23 14:19:18 +0300

    Inline Images

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var inlineimage = require('gulp-inline-image');
 
 gulp.task('sass', function () {
   gulp.src('./src/*.scss')
     .pipe(sass())
+    .pipe(inlineimage())
     .pipe(gulp.dest('./build'));
 });
 
